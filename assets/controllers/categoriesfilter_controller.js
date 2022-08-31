2eb771704e41e9584dc62bbadebb718fc8592ff3
Aubin <couscous_tachete@hotmail.fr>
2022-09-01 00:42:21 +0200

    creating categories filter with js and stimulus

---
+++ assets/controllers/categoriesfilter_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+
+    static targets = ['filter', 'category'];
+
+
+    categories_filter() {
+        let category, txtValue, string, input;
+
+        input = this.filterTarget;
+        string = input.value.toUpperCase();
+
+        for (let i=0; i < this.categoryTargets.length; i++) {
+            category = this.categoryTargets[i];
+            txtValue = category.textContent || category.innerText;
+            if (txtValue.toUpperCase().indexOf(string) > -1) {
+                category.style.display = "";
+            } else {
+                category.style.display = "none";
+            }
+        }
+
+
+        //this.categoryTargets[1].innerHTML = "test";
+
+        //console.log("test", this.categoryTarget);
+    }
+}
